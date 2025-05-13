0316a423fcb67590ddb83243304a06848b06875c
vig-2004 <pothugantivigneshvishu@gmail.com>
2025-05-14 00:57:01 +0530

    Updated project structure and files

---
+++ src/TaskCard.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import "./TaskCard.css";
+const TaskCard=(props)=>{
+    let x="Due on: "+props.dueDate;
+    if(!props.dueDate){
+        x="Completed ok: "+props.completedAtDate;
+    }
+    return(
+        <div className="TaskItem">
+            <h1 className="text-m font-bold">{props.title}</h1>
+            <p>{x}</p>
+            <p>assignee : {props.assigneeName}</p>
+        </div>
+    )
+}
+export default TaskCard;
